8e53c8c503a62c635bd60ed61c8ed4b0ca6241ba
JooYoung <jy_23@JooYoung.local>
2024-03-20 19:49:21 +0900

    feat: Url 클래스 요구사항 및 자료구조 정의
    요구사항
    1. request uri 파싱
    2. request 유효성 검사 ? 에러 코드 반환 방법
    3. 리소스 정보 획득 ?
    4. static 클래스 여부 결정 ?
    
    void ParseUriComponent(std::string& request_uri);
    void ReconstructTargetUri();
    
    자료구조
    URI Component
    Path Component
    
    target uri = scheme "://" host [:port] "/" path component [? query]
    request target form = origin-form, absolute-form, authority-form, asterisk-form
    
    To Do
    리소스 정보 포함/분리 여부
    요구사항 처리 메서드 구현
    자료구조 구체
    
    Fixes: #12

--- src/url.hpp
+++ src/url.hpp
@@ -1,14 +1,64 @@
 #ifndef URL_HPP
 #define URL_HPP
 
+#include <iostream>
+#include <list>
+#include <map>
+
+/*
+        1. request uri 파싱
+        2. request 유효성 검사 ? 에러 코드 반환 방법
+        3. 리소스 정보 획득 ?
+        4. static 클래스 여부 결정 ?
+*/
+
 class Url {
  private:
+  enum RequestTargetFrom {
+    kOriginForm,
+    kAbsoluteForm,
+    kAuthorityForm,
+    kAsteriskForm
+  };
+
+  /*
+        todo - ResourceType 타입과 변수의 선언 위치 결정 필요
+   */
+  enum ResourceType { kFile, kDirectory, kGateway };
+  ResourceType resource_type_;
+
+  struct PathComponent {
+    std::string path;
+    std::map<std::string, std::string> parameter;
+  };
+
+  /*
+        URI Component의 구성요소
+  */
+  std::string scheme_;
+  std::string host_;
+  int port_;  // todo - socket API의 port 자료구조로 변경
+  std::list<PathComponent> path_component_;  // todo - 자료구조 결정 필요
+  std::map<std::string, std::string> query_string_;
+
+  std::string target_uri_;
+  RequestTargetFrom request_target_form_;
+  // todo - 서버 정보를 저장할 변수 필요
+
   Url();
   Url(const Url& obj);
   ~Url();
   Url& operator=(const Url& obj);
 
  public:
+  void ParseUriComponent(std::string& request_uri);
+  // todo - Impl method, URI 컴포넌트의 값을 채운다.
+  void ReconstructTargetUri();
+  /*
+        todo - Impl method
+        파싱된 컴포넌트 정보를 이용해 target URI를 생성한다.
+        response에 쓰이지는 않을 것 같음
+  */
 };
 
 #endif
