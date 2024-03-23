b5df805746dc0876f8ab7150fcd192373adb1b0c
JooYoung <jy_23@JooYoung.local>
2024-03-24 00:09:55 +0900

    feat: pct-encoded ABNF Rule 함수화
    기존 상위 ABNF 룰에서 동작하는 코드를 독립적으로 함수화한다.
    
    Fixes: #17

--- src/abnf.cpp
+++ src/abnf.cpp
@@ -39,3 +39,14 @@ bool Abnf::IsSubDlims(char c) {
       return false;
   }
 }
+
+char Abnf::DecodePctEncoded(std::string& s, const size_t pos) {
+  if (pos + 2 >= s.length() || !std::isxdigit(s[pos + 1]) ||
+      !std::isxdigit(s[pos + 2])) {
+    return -1;
+  } else {
+    char c = strtol(s.substr(pos + 1, pos + 3).c_str(), NULL, 16);
+    s.replace(pos, pos + 2, std::string(1, c));
+    return c;
+  }
+}
--- src/abnf.hpp
+++ src/abnf.hpp
@@ -2,12 +2,14 @@
 #define ABNF_HPP
 
 #include <cctype>
+#include <iostream>
 
 class Abnf {
  private:
  public:
-  static bool IsUnreserved(char c);
-  static bool IsSubDlims(char c);
+  static bool IsUnreserved(const char c);
+  static bool IsSubDlims(const char c);
+  static char DecodePctEncoded(std::string& s, const size_t pos);
 };
 
 #endif
